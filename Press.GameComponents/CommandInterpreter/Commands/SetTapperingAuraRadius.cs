7f2fba33bc11dd78d17539b50d78e0ea9b34308d
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:31:35 +0500

    add: set tappering aura radius command

---
+++ Press.GameComponents/CommandInterpreter/Commands/SetTapperingAuraRadius.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Press.GameComponents.Properties;
+
+namespace Press.GameComponents
+{
+    public class SetTapperingAuraRadius : Command
+    {
+        public override string Name { get; set; } = "auraRadius";
+
+        public override void Run(List<string> arguments, object processingObject = null) =>
+            Settings.Default.TapperingAuraRadius = Convert.ToDouble(arguments[0]);
+    }
+}
