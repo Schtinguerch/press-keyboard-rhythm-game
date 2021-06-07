b3b39a7ddca7f4a97008431eff9d99027d3e9c5d
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:27:03 +0500

    add: interaction logic for test GUI

--- Press.GameClient/MainWindow.xaml.cs
+++ Press.GameClient/MainWindow.xaml.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Press.GameComponents;
+using ComponentSetup = Press.GameComponents.Properties.Settings;
+using ClientSetup = Press.GameClient.Properties.Settings;
 
 namespace Press.GameClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private GameScene _scene;
+            
         public MainWindow()
         {
             InitializeComponent();
+            _scene = new GameScene(GameSceneCanvas, ComponentSetup.Default.OpenedMapFilePath);
+        }
+
+        private void MainWindowOnKeyDown(object sender, KeyEventArgs e)
+        {
+            Test.Text = _scene.PointsFromTap(e);
         }
     }
 }
