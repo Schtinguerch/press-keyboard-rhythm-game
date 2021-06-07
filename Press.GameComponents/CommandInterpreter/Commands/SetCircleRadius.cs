5d4926c5ce1c98c2a71ab0f6b569c4b3824a75a5
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:30:35 +0500

    add: Set circle radius command

---
+++ Press.GameComponents/CommandInterpreter/Commands/SetCircleRadius.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Press.GameComponents.Properties;
+
+namespace Press.GameComponents
+{
+    public class SetCircleRadius : Command
+    {
+        public override string Name { get; set; } = "circleRadius";
+
+        public override void Run(List<string> arguments, object processingObject = null) =>
+            Settings.Default.CircleRadius = Convert.ToDouble(arguments[0]);
+    }
+}
