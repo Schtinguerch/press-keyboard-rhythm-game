dccc450abcc29c708da952b3251317f75f271f5e
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:32:55 +0500

    upd:  - added the reference to mutalble object to edit  - refactoring the empty or null conditions

--- Press.GameComponents/CommandInterpreter/Command.cs
+++ Press.GameComponents/CommandInterpreter/Command.cs
@@ -6,16 +6,16 @@ namespace Press.GameComponents
     {
         public abstract string Name { get; set; }
 
-        public abstract void Run(List<string> arguments);
+        public abstract void Run(List<string> arguments, object processingObject = null);
 
         protected void SetAdditional(List<string> arguments)
         {
-            if (arguments != null)
-            {
-                SubCommandName = (arguments.Count > 0) ? arguments[0] : "";
-                SubCommandArguments = arguments.SubList(0);
-                UnitedStringArgument = arguments.ConcatToString();
-            }
+            if (arguments == null) return;
+            if (arguments.Count == 0) return;
+
+            SubCommandName = arguments[0];
+            SubCommandArguments = arguments.SubList(0);
+            UnitedStringArgument = arguments.ConcatToString();
         }
 
         protected string UnitedStringArgument;
