0ed6ba20f30b113b025d6d281e255997ab6d47dc
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:30:05 +0500

    add: Command set command, calls other setter-commands

--- Press.GameComponents/CommandInterpreter/Commands/CommandSet.cs
+++ Press.GameComponents/CommandInterpreter/Commands/CommandSet.cs
@@ -7,20 +7,20 @@ namespace Press.GameComponents
     {
         public override string Name { get; set; } = "set";
 
-        public override void Run(List<string> arguments)
+        public override void Run(List<string> arguments, object processingObject = null)
         {
             SetAdditional(arguments);
+            if (arguments.Count < 2) return;
 
-            if (arguments.Count >= 2)
+            var commands = new List<Command>()
             {
-                var commands = new List<Command>()
-                {
-                    new SetShowTime()
-                };
+                new SetShowTime(),
+                new SetCircleRadius(),
+                new SetTapperingAuraRadius(),
+            };
 
-                var subInterpreter = new CommandInterpreter(commands);
-                subInterpreter.Run(SubCommandName, SubCommandArguments);
-            }
+            var subInterpreter = new CommandInterpreter(commands);
+            subInterpreter.Run(SubCommandName, SubCommandArguments);
         }
     }
 }
