c23aa803d17eefe140b8478ce605a33ec8e77a6c
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:29:10 +0500

    add: Map-preprocessor command, includes code from other files like #include in C/C++

---
+++ Press.GameComponents/CommandInterpreter/Commands/CommandInclude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace Press.GameComponents
+{
+    class CommandInclude : Command
+    {
+        public override string Name { get; set; } = "include";
+
+        public override void Run(List<string> arguments, object processingObject = null)
+        {
+            SetAdditional(arguments);
+
+            if (UnitedStringArgument.LastChar() != 's')
+                UnitedStringArgument = UnitedStringArgument.Substring(0, UnitedStringArgument.Length - 1);
+
+            var includingCode = File.ReadAllText(UnitedStringArgument).PreprocessedMap();
+            var processingCode = (TextBlock)processingObject;
+
+            processingCode.Text = processingCode.Text.Replace($"{Name} {UnitedStringArgument}", includingCode);
+        }
+    }
+}
