52a02f02fb370e389311a15e4a34126979842020
Schtinguerch <schtinguerch@gmail.com>
2021-06-07 20:33:39 +0500

    upd-add: editing some object by commands

--- Press.GameComponents/CommandInterpreter/CommandInterpreter.cs
+++ Press.GameComponents/CommandInterpreter/CommandInterpreter.cs
@@ -8,26 +8,45 @@ namespace Press.GameComponents
     {
         private List<Command> _commands;
 
+        private object _processingObject;
+
         public List<Command> Commands { get => _commands; }
 
-        public CommandInterpreter() => 
+        public CommandInterpreter(object processingObject = null)
+        {
             _commands = new List<Command>();
+            _processingObject = processingObject;
+        }
 
-        public CommandInterpreter(List<Command> commands) =>
+
+        public CommandInterpreter(List<Command> commands, object processingObject = null)
+        {
             _commands = commands;
+            _processingObject = processingObject;
+        }
 
-        public CommandInterpreter(Command[] commands) =>
+        public CommandInterpreter(Command[] commands, object processingObject = null)
+        {
             _commands = commands.ToList();
+            _processingObject = processingObject;
+        }
+            
 
         public void Run(string commandName, string[] arguments) =>
-            Run(commandName, arguments.ToList());
+            Run(commandName, arguments.ToList(), _processingObject);
 
-        public void Run(string commandName, List<string> arguments)
+        public void Run(string expression)
+        {
+            var tokens = expression.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Run(tokens[0], tokens.SubList(0), _processingObject);
+        }
+
+        public void Run(string commandName, List<string> arguments, object processingObject = null)
         {
             foreach (var command in _commands)
                 if (commandName == command.Name)
                 {
-                    command.Run(arguments);
+                    command.Run(arguments, processingObject);
                     break;
                 }
         }
